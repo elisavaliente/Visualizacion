1c6355b04f45943832379d7672a9ed84054c33e8
elisavalientecunef <elisa.valiente@cunef.edu>
2019-01-15 17:46:54 +0100

    lakjdalksdksak

--- ExplanationGit.R
+++ ExplanationGit.R
@@ -1,2 +1,5 @@
 # gitignore: fichero que permite ignorar cosas. 
 # En el environment puedes ver que ahora hay una pestaña más que se llama git.
+# cuando haces un commit la congelas y cualquier cosa que modificas no se borra la anterior, es como una copia de seguridad.
+
+# holi caracoli
