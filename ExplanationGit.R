322f64b55efd08d03c24c604e71bce3d2e5f1c81
elisavalientecunef <elisa.valiente@cunef.edu>
2019-01-15 17:49:14 +0100

    He avanzado

--- ExplanationGit.R
+++ ExplanationGit.R
@@ -2,4 +2,6 @@
 # En el environment puedes ver que ahora hay una pestaña más que se llama git.
 # cuando haces un commit la congelas y cualquier cosa que modificas no se borra la anterior, es como una copia de seguridad.
 
-# holi caracoli
+# función SHA es una cosa para identificar cosas, los commits de todo el mundo tienen un SHA para siempre ese único. 
+
+# puchi puchi 
